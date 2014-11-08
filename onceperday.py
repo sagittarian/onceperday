4c9c697b60bceb641ab08e5633424951c75c9749
Adam Mesha <adam@mesha.org>
2014-11-08 21:22:43 +0200

    Pass the timestamp to the once-per-day function.

--- onceperday.py
+++ onceperday.py
@@ -72,8 +72,9 @@ class OncePerDay:
 		today = datetime.date.today()
 		if self.checkcache(today, magic_point):
 			return
+		timestamp = datetime.datetime.combine(today, magic_point)
 		try:
-			result = self.func()
+			result = self.func(timestamp)
 		except Exception as e:
 			# if the function raises an exception, we don't caunt it as having been done
 			print('Exception: {}'.format(e))
